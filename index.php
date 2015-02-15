24943b37704185fe14d71db63d95fdf5b1a9f267
Taylor Smith <taylor.smith84@gmail.com>
2015-02-15 11:43:17 -0500

    updates

--- index.php
+++ index.php
@@ -31,54 +31,35 @@
 <div id="main3">
   
   <h2 class="cent">Recent Projects</h2>
+
+      <?php
+          $args = array(
+              'post_type' => 'project',
+              'posts_per_page' => 3,
+          );
+  
+          $query = new WP_Query( $args );
+          while($query->have_posts()) : $query->the_post();
+          $project_count = 0;
+          $project_count++;
+      ?>
   <div class="lefty">
       <div class="row-featurette">
+
         <div class="col-md-7">
-          <h2 class="featurette-heading">Web Design I - Final <span class="text-muted"><br>December, 2014</span></h2>
-          <p class="lead">I created a travel site for my web design final with a slide menu and transitions all done in CSS3 and HTML5 with original graphics. The use of responsive design is the biggest factor in having an effective website.</p>
+          <h2 class="featurette-heading"><?php the_title();?> <span class="text-muted"><br>December, 2014</span></h2>
+          <p class="lead"><?php the_excerpt();?></p>
         </div>
         <div class="col-md-5">
           <img class="featurette-image img-responsive" src="<?php echo $theme_path; ?>/images/port_img/final_desk.png" alt="Final - Travel Site">
         </div>
       </div>
-</div>
+  </div>
+
       <hr class="featurette-divider">
-<div class="righty">
-      <div class="row-featurette">
-        <div class="col-md-5-mid-img">
-          <img class="featurette-image img-responsive" src="<?php echo $theme_path; ?>/images/port_img/midterm_desk.png" alt="Midterm - Photography Site">
-        </div>
-        <div class="col-md-7-right">
-          <h2 class="featurette-heading">Web Design I - Midterm <span class="text-muted"><br>October, 2014</span></h2>
-          <p class="lead">My first major website of the year was for a made up client looking for a photography site. Done in HTML5 and CSS3 this site highlights the use of web fonts and responsive design.</p>
-        </div>
-      </div>
-</div>
-      <hr class="featurette-divider">
-<div class="lefty">
-      <div class="row-featurette">
-        <div class="col-md-7">
-          <h2 class="featurette-heading"> Pink Floyd CD Redesign <span class="text-muted"><br>April, 2014</span></h2>
-          <p class="lead">This was a CD redesign project I did in the Design Studies program. The logo was done with pencil crayon, pastels, and the pen tool in Adobe Illustrator. This project included printing and mounting the CD label, a front, back, spine, inside tray and door graphics.</p>
-        </div>
-        <div class="col-md-5">
-          <img class="featurette-image img-responsive" src="<?php echo $theme_path; ?>/images/port_img/floyd_redesign_desk.png" alt="Pink Floyd">
-        </div>
-      </div>
-</div>
-      <hr class="featurette-divider">
-<div class="righty">
-      <div class="row-featurette">
-        <div class="col-md-5-mid-img">
-          <img class="featurette-image img-responsive" src="<?php echo $theme_path; ?>/images/port_img/personal_blog_desk.png" alt="Movable Type">
-        </div>
-        <div class="col-md-7-right">
-          <h2 class="featurette-heading">Personal Blog <span class="text-muted"><br>2013</span></h2>
-          <p class="lead">I created this blog with Movable Type blog software. By editing PHP templates and filling it with my own images and content, I was able to manipulate the layout much the same as Wordpress using XHTML and CSS.</p>
-        </div>
-    </div>
-</div>
-<hr class="featurette-divider">
+
+<?php endwhile; ?>
+
 		<h2 class="cent2">Contact</h2>
 
 	<p class="cont">
